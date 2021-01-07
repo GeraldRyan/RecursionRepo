913c4adae1006a46742a4bdbc46b0630694fef71
GeraldRyan <gerald.ryan40@yahoo.com>
2021-01-07 17:22:29 -0600

    learning to build trees, then graphs and fancy trees. what better way to understand algorithms and code than ability to build

--- tree/tree_init.c
+++ tree/tree_init.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int tree_values[] = {11, 71, 35, 85, 34, 26, 48, 95, 57, 82, 15, 35};
+
 typedef struct Node
 {
   int value;
@@ -20,7 +23,7 @@ typedef struct Tree
   Node *root;
 } Tree;
 
-Node *new_node(int value)
+Node *create_new_node(int value)
 {
   struct Node *node = malloc(sizeof(struct Node));
   node->value = value;
@@ -30,38 +33,51 @@ Node *new_node(int value)
 int free_node(Node *node)
 {
   free(node);
-  return 0;
+  return 1;
 }
 
 int addLevel(Node *node)
 {
-  node->left = new_node(node->value + 1);
-  node->right = new_node(node->value + 2);
+  node->left = create_new_node(node->value + 1);
+  node->right = create_new_node(node->value + 2);
+}
+
+int insert_node_binary_tree(struct Tree* tree, int value){
+
+  Node* new_node = create_new_node(value);
+
+
+
+
+  return 1;
 }
 
 // int grow_tree_recursively(Node* node){
 
 // };
 
-Tree* make_tree(void)
+Tree* make_tree(int* values)
 {
 
   // set up tree
   int newlevels = 2;
   Tree *tree = malloc(sizeof(Tree));
-  tree->root = new_node(0);
-  Node *root = tree->root;
-  // How do make this recursive, and truly ordered?!
-  addLevel(root);
-  addLevel(root->left);
-  addLevel(root->right);
-  addLevel(root->left->left);
-  addLevel(root->left->right);
-  addLevel(root->right->left);
-  addLevel(root->right->right);
+  Node* root = create_new_node(50);
+  tree->root = root;
+  tree->numNodes++;
 
-  Node *rootl = root->left;
-  Node *rootr = root->right;
+  for (int i )
+
+  // addLevel(root);
+  // addLevel(root->left);
+  // addLevel(root->right);
+  // addLevel(root->left->left);
+  // addLevel(root->left->right);
+  // addLevel(root->right->left);
+  // addLevel(root->right->right);
+
+  // Node *rootl = root->left;
+  // Node *rootr = root->right;
 
   // for (int i = 0; i < newlevels; i++)
   // {
--- tree_node_count.c
+++ tree_node_count.c
@@ -2,15 +2,45 @@
 #include <stdlib.h>
 #include "./tree/tree_init.c"
 
-int main(void){
+int count_children(Node *node, int tally)
+{
+  if (node == NULL)
+  {
+    return 0 + tally;
+  } // BASE CASE
 
-  Tree* tree = make_tree();
-  Node* node = tree->root;
+  else if (node->left == NULL && node->right == NULL)
+  {
+    return 0 + tally;
+  }
+  else if (node->left == NULL && node->right != NULL)
+  {
+    count_children(node->right, tally);
+    return 1 + tally;
+  }
+  else if (node->right == NULL && node->left != NULL)
+  {
+    count_children(node->left, tally);
+    return 1 + tally;
+  }
+  else
+  {
+    int left = count_children(node->left, tally);
+    int right = count_children(node->right, tally);
+    return 2 + tally + left + right;
+  }
+}
+
+int main(void)
+{
+  Tree *tree = make_tree();
+  Node *node = tree->root;
+  int node_count = 0;
   printf("Tree root node value %d \n", node->value);
   printf("Tree first left value %d \n", node->left->value);
   printf("Tree first right value %d \n", node->right->value);
-  
-
+  node_count = count_children(node, 1);
+  printf("Node count is %d\n", node_count);
 
   return 1;
 }
